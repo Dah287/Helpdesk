4be0f5bf45f6bab1d09aa258c61fbc2a86f73b19
Dah287 <dahmanb996@gmail.com>
2025-08-11 15:01:09 +0100

    v-avant-spring-scurity3

---
+++ src/main/java/com/example/helpdesk/JwtKeyGenerator.java
@@ -0,0 +1,15 @@
+package com.example.helpdesk.util;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Base64;
+
+public class JwtKeyGenerator {
+    public static void main(String[] args) {
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
+        System.out.println("Generated Key: " + encodedKey);
+    }
+}
